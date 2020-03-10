28df3fb14f6c31aeb3425d144e0abc656cdb7435
Punal Chotrani <punalsc@gmail.com>
2020-03-10 11:30:17 +0000

    Updated function

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,4 +1,8 @@
-fetch('../../../src/js/data/1602593.json')
+// import dataJSON from '../js/data/1602593.json';
+
+fetch(
+  'https://gist.githubusercontent.com/punalsc/b74313826cc464b1400524e319c68ed1/raw/6a7c25837f74243443a3d518118a74f78e02d70b/cardone.json'
+)
   .then(response => {
     return response.json();
   })
